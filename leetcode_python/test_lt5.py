15076ff970b07e0fde04a88475ae03a01afacb71
Meng Zhao <zhaomeng@singularityai.cn>
2022-04-28 09:39:38 +0800

    restructure to use vscode's pytest plugin

--- leetcode_python/test_lt5.py
+++ leetcode_python/test_lt5.py
@@ -2,33 +2,36 @@ class Solution:
     def longestPalindrome(self, s: str) -> str:
         # 想法：以每个index为中心，从这个中心往外找，看是不是palindrome
         longest = 0
-        longest_palindrome = ''
-        def search_palindrome(i: int):
+        longest_palindrome = ""
+
+        def search_palindrome(i: int, even: bool = False):
+            nonlocal longest_palindrome
+            nonlocal longest
             offset = 0
             while True:
                 left = i - offset
-                right = i + offset + 1
+                right = i + offset + int(even)  # 通过这里切换是考虑以i为中心，还是以i和i+1一起为中心
                 length = right - left + 1
                 if left > 0 and right < len(s) and s[left] == s[right]:
                     if length > longest:
-                        longest_palindrome = s[left, right]
+                        longest_palindrome = s[left : (right + 1)]
                         longest = length
-                    continue
                 else:
                     break
-
                 offset += 1
-        
+
         for i in range(len(s)):
-            search_palindrome(i)
-        
+            search_palindrome(i, False)
+            search_palindrome(i, True)
+
         return longest_palindrome
 
 
 def test_solution():
     examples = [
-        ("babad", "bab"),
+        ("babad", ["bab", "aba"]),
         ("cbbd", "bb"),
     ]
     for input, answer in examples:
-        assert Solution().longestPalindrome(input) == answer
+        res = Solution().longestPalindrome(input)
+        assert res == answer or res in answer
