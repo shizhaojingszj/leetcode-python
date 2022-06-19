375eb9118e560969981c50a7fc2a306262decffb
Meng Zhao <zhaomeng@singularityai.cn>
2022-06-19 11:07:39 +0800

    lt424 passed but too slow!!!
    Solution1: 426ms
    Solution2: 892ms

---
+++ leetcode_python/test_lt424.py
@@ -0,0 +1,156 @@
+from collections import defaultdict
+from operator import le
+from unittest import result
+
+
+class SlidingWindow:
+    def __init__(self, s, k):
+        """A class represent all logic in SlidingWindow.
+
+        Args:
+            s (str): string input
+            k (str): max number of replacement
+        """
+        self.N = len(s)
+        self.s = s
+        self.k = k
+        # left pointer
+        self.left = 0
+        # right pointer
+        self.right = 0
+        # counter
+        self.counter = defaultdict(lambda: 0)
+        # max character
+        self.max_char = None
+        self.max_char_freq = 0
+        # result
+        self.max_length = 0
+        # debug
+        self.step_counter = 0
+
+    @property
+    def length(self):
+        return self.right - self.left
+
+    def left_forward(self):
+        # 左指针向右移动
+        if self.left == self.N - 1:
+            raise StopIteration("left doesnot need to forward again")
+        # sliding window最左边被排除出范围的char
+        char = self.s[self.left]
+        self.counter[char] -= 1
+        self.max_char, self.max_char_freq = self.get_max_minus(char)
+        self.left += 1
+
+    def get_max_minus(self, some=None):
+        if not some:
+            return self.left[0], 1
+        else:
+            if self.max_char == some:
+                return sorted(self.counter.items(), key=lambda x: x[1], reverse=True)[0]
+            else:
+                return [self.max_char, self.max_char_freq]
+
+    def get_max_plus(self, some: str):
+        assert len(some) == 1, some
+        if self.max_char == some:
+            return [self.max_char, self.max_char_freq + 1]
+        else:
+            if self.counter[some] > self.max_char_freq:
+                return [some, self.counter[some]]
+            else:
+                return [self.max_char, self.max_char_freq]
+
+    def right_forward(self):
+        # 右指针向右移动
+        if self.right == self.N:
+            # 此时右指针已经移出s了
+            raise StopIteration("right out of bounds")
+        # 新进入sliding window的char
+        char = self.s[self.right]
+        self.counter[char] += 1
+        self.max_char, self.max_char_freq = self.get_max_plus(char)
+        self.right += 1
+        now = self.counter[self.max_char] + self.k
+        if now >= self.length:
+            self.max_length = max(self.length, self.max_length)
+
+    def step(self):
+        self.step_counter += 1
+        if self.length > self.max_length:
+            self.left_forward()
+        else:
+            self.right_forward()
+
+    def logic(self):
+        while self.left < self.N and self.right <= self.N:
+            try:
+                self.step()
+            except StopIteration as e:
+                print(e.args)
+                break
+            except Exception as e:
+                print(e.args)
+                raise e
+        print("result", self.max_length)
+        return self.max_length
+
+
+class Solution1:
+    def characterReplacement(self, s: str, k: int) -> int:
+
+        sw = SlidingWindow(s, k)
+        result = sw.logic()
+        return result
+
+
+input1 = [
+    (("ABAA", 0), 2),
+    (("ABAB", 2), 4),
+]
+
+
+def test_solution1():
+    # ex. 1
+    for input, expect in input1:
+        res = Solution1().characterReplacement(*input)
+        assert res == expect, (res, expect)
+
+
+class Solution2:
+    def characterReplacement(self, s: str, k: int) -> int:
+        # 数据结构
+        window = defaultdict(lambda: 0)
+        saved_max_length = 0
+
+        # 往右走
+        left, right, N, K = 0, 0, len(s), k
+
+        while right <= N - 1:
+            c = s[right]
+            window[c] += 1
+            right += 1
+            length = right - left
+            max_length = self.max_char(window)
+            # 当条件不符合题目时
+            while max_length + K < length and left <= N - 1:
+                # 不符合条件时，修改left
+                c1 = s[left]
+                left += 1
+                window[c1] -= 1
+                # TODO: 为什么这里不用再计算了？
+                max_length = self.max_char(window)
+                length = right - left
+            if max_length + K >= length:
+                saved_max_length = max(length, saved_max_length)
+
+        return saved_max_length
+
+    def max_char(self, window: defaultdict):
+        return max([i for i in window.items()], key=lambda x: x[1])[1]
+
+
+def test_solution2():
+    for input, expect in input1:
+        res = Solution2().characterReplacement(*input)
+        assert res == expect, (res, expect)
